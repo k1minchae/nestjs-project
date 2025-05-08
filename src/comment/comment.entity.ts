ef0f9c557eee2e83a7f0339fc49f2dc91254ba32
kiminchae <alsco9707@gmail.com>
2025-05-08 18:28:59 +0900

    feat : 대댓글 관계설정

--- src/comment/comment.entity.ts
+++ src/comment/comment.entity.ts
@@ -1,10 +1,12 @@
 import { Post } from 'src/post/post.entity';
+import { User } from 'src/user/user.entity';
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
   ManyToOne,
   JoinColumn,
+  OneToMany,
 } from 'typeorm';
 
 @Entity('Comment')
@@ -12,8 +14,9 @@ export class Comment {
   @PrimaryGeneratedColumn({ type: 'bigint' })
   comment_id: number;
 
-  @Column()
-  user_id: number;
+  @ManyToOne(() => User, (user) => user.posts, { eager: false })
+  @JoinColumn({ name: 'user_id' }) // 외래키 컬럼 명시
+  user: User;
 
   @ManyToOne(() => Post, (post) => post.comments)
   @JoinColumn({ name: 'post_id' })
@@ -21,6 +24,13 @@ export class Comment {
 
   @Column({ nullable: true })
   parent_comment_id: number;
+
+  @ManyToOne(() => Comment, (comment) => comment.replies, { nullable: true })
+  @JoinColumn({ name: 'parent_comment_id' })
+  parent: Comment;
+
+  @OneToMany(() => Comment, (comment) => comment.parent)
+  replies: Comment[];
 
   @Column({ length: 1000 })
   comment_content: string;
