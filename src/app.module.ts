a32e29dbbc7bbbb7fa49531020b75ad63764d874
kiminchae <alsco9707@gmail.com>
2025-05-08 10:01:20 +0900

    init: 프로젝트 세팅

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // true 지정 시 다른 모듈에서 import 하지 않고 바로 사용 가능
+      envFilePath: ['.env.development', '.env.development.local'], // 접근 가능한 환경변수 목록
+    }),
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT ?? '5432'), // 기본값 5432
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD, // 반드시 string 이어야 함
+      database: process.env.PG_DBNAME,
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
