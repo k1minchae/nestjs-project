8c43ec49696aee7db549007f47b71683cba25ab3
kiminchae <alsco9707@gmail.com>
2025-05-08 18:41:48 +0900

    chore : 레포지토리 의존성 주입

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -6,10 +6,13 @@ import { Images } from 'src/files/image.entity';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { User } from 'src/user/user.entity';
+import { Like } from 'src/like/like.entity';
+import { Comment } from 'src/comment/comment.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Post, Files, Images]),
+    TypeOrmModule.forFeature([Post, Files, Images, User, Like, Comment]),
     forwardRef(() => AuthModule),
   ],
   providers: [PostService],
