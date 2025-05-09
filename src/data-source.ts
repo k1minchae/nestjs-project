d738629048a2937417cc35dfb65626b920809a9a
kiminchae <alsco9707@gmail.com>
2025-05-09 17:40:32 +0900

    chore : 도커 설정

---
+++ src/data-source.ts
@@ -0,0 +1,12 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: Number(process.env.DATABASE_PORT) || 5432,
+  username: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'board_db',
+  entities: [__dirname + '/../**/*.entity.{ts,js}'],
+  synchronize: true,
+});
